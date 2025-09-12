ab67072f5392a461386468273584fd19bc02a7f0
abcdegon <egon@gruenter.eu>
2025-09-12 14:31:17 +0200

    New version

--- index.md
+++
@@ -1,9 +0,0 @@
-Tach zesamme, I'm Abcd Egon!
-
-I am a security expert living in [Rurich](https://de.wikipedia.org/wiki/Rurich). Until 2023 I workes for 25 years at [Juelich Supercomputing Center](https://www.fz-juelich.de/ias/jsc). A list of my publications can be found [here](https://orcid.org/0000-0001-6708-2109).
-
-This web site is a small smorgasbord of different things.
-
-You can download some slides here about different topics. Note: Everything here is licensed under CC BY 4.0. To view a copy of this license, visit [https://creativecommons.org/licenses/by/4.0/](https://creativecommons.org/licenses/by/4.0).
-
-
---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+// Store a reference to the <h1> in a variable
+let myHeading = document.querySelector("h1");
+// Update the text content of the <h1>
+myHeading.textContent = "Hello world!";
+
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox2.png") {
+    myImage.setAttribute("src", "images/air.jpg");
+  } else {
+    myImage.setAttribute("src", "images/firefox2.png");
+  }
+});
+
+let myButton = document.querySelector("button");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {  setUserName();});
